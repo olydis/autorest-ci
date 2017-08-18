cc2ca84910d43e9a9b1aa2d0805c7d9615090676
Johannes Bader <jobader@microsoft.com>
2017-08-18 14:54:45 -0700

    iterate

--- app.ts
+++ app.ts
@@ -1,18 +1,31 @@
-import * as request_plain from "request-promise-native";
+#!/usr/bin/env node
+
+import { GitHubCiClient, PullRequest } from './github';
 import { arch, platform, release, tmpdir } from "os";
 import { join } from "path";
-import * as simpleGit from "simple-git/promise";
-import * as mkdir from "mkdirp-promise";
+import * as simpleGit from 'simple-git/promise';
+import * as mkdir from 'mkdirp-promise';
 import { exec } from "child_process";
 
 // config
-const githubOwner = "Azure";
-const githubRepo = "autorest.common";
-const githubToken = process.env.GITHUB_TOKEN;
 const ciIdentifier = `${platform()}-${arch()}`;
+const githubOwner = "Azure";
+const githubRepos = [
+  "autorest.common",
+  "autorest.modeler",
+  "autorest.azureresourceschema",
+  "autorest.csharp",
+  "autorest.go",
+  "autorest.java",
+  "autorest.nodejs",
+  "autorest.php",
+  "autorest.ruby",
+  "autorest.python",
+  "autorest.testserver"
+];
+const githubToken = process.env.GITHUB_TOKEN;
 const ciStatusTimeoutMs = 1000 * 60 * 5; // 5min
 const jobUid = "CI" + Math.random().toString(36).substr(2, 5);
-const statusPrefix = `[${jobUid}] `
 const tmpFolder = join(tmpdir(), jobUid);
 
 if (!githubToken) {
@@ -20,78 +33,8 @@ if (!githubToken) {
   process.exit(1);
 }
 
-// helpers
-const request = request_plain.defaults({
-  headers: {
-    "User-Agent": "AutoRest CI",
-    "Authorization": "token " + githubToken
-  }
-});
-
 const delay = (ms: number): Promise<void> => new Promise<void>(res => setTimeout(res, ms));
-
-// GitHub
-type PullRequest = { updatedAt: Date, number: number, title: string, baseRef: string, baseID: string, headID: string };
-function parsePR(pr: any): PullRequest {
-  return {
-    updatedAt: new Date(pr.updated_at),
-    number: pr.number,
-    title: pr.title,
-    baseRef: pr.base.ref,
-    baseID: pr.base.sha,
-    headID: pr.head.sha
-  };
-}
-async function getPullRequests(): Promise<PullRequest[]> { // https://developer.github.com/v3/pulls/#list-pull-requests
-  return JSON.parse(await request.get(`https://api.github.com/repos/${githubOwner}/${githubRepo}/pulls`)).map(parsePR);
-}
-async function getPullRequest(prNumber: number): Promise<PullRequest> { // https://developer.github.com/v3/pulls/#get-a-single-pull-request
-  return parsePR(JSON.parse(await request.get(`https://api.github.com/repos/${githubOwner}/${githubRepo}/pulls/${prNumber}`)));
-}
-type State = "success" | "pending" | "failure";
-type Status = { updatedAt: Date, state: State, description: string, url: string };
-type Statuses = { [jobName: string]: Status };
-async function getPullRequestStatuses(pr: PullRequest): Promise<Statuses> { // https://developer.github.com/v3/repos/statuses/#get-the-combined-status-for-a-specific-ref
-  const res = await request.get(`https://api.github.com/repos/${githubOwner}/${githubRepo}/commits/${pr.headID}/status`);
-  const statuses = JSON.parse(res).statuses;
-  const result: Statuses = {};
-  for (const status of statuses) {
-    result[status.context] = {
-      updatedAt: new Date(status.updated_at),
-      state: status.state,
-      description: status.description,
-      url: status.target_url
-    };
-  }
-  return result;
-}
-async function setPullRequestStatus(pr: PullRequest, state: State, description: string, url?: string): Promise<void> { // https://developer.github.com/v3/repos/statuses/#create-a-status
-  const body: any = {};
-  body.state = state;
-  if (url) body.target_url = url;
-  if (description) body.description = statusPrefix + description;
-  body.context = ciIdentifier;
-  const res = await request.post(`https://api.github.com/repos/${githubOwner}/${githubRepo}/statuses/${pr.headID}`, { body: JSON.stringify(body) });
-}
-
-// app
-
 function log(x: any): void { console.log(x); }
-
-async function getJobStatus(pr: PullRequest): Promise<Status | undefined> {
-  const statuses = await getPullRequestStatuses(pr);
-  return statuses[ciIdentifier];
-}
-
-async function isLastStatusOurs(pr: PullRequest): Promise<boolean> {
-  const status = await getJobStatus(pr);
-  console.log(status.description);
-  return status && status.description.startsWith(statusPrefix);
-}
-
-async function wasUpdated(pr: PullRequest): Promise<boolean> {
-  return (await getPullRequest(pr.number)).updatedAt.getTime() !== pr.updatedAt.getTime();
-}
 
 function runCommand(command: string, cwd: string): [() => string, Promise<Error | null>] {
   let output: string = "";
@@ -107,7 +50,7 @@ function runCommand(command: string, cwd: string): [() => string, Promise<Error 
   })];
 }
 
-async function runJob(pr: PullRequest): Promise<void> {
+async function runJob(ghClient: GitHubCiClient, pr: PullRequest): Promise<void> {
   try {
     const jobFolder = join(tmpFolder, new Date().toISOString().replace(/[:-]/g, "").split('.')[0] + "_" + pr.number);
     log("   - creating workspace");
@@ -115,23 +58,23 @@ async function runJob(pr: PullRequest): Promise<void> {
 
     // git
     const timeStamp1 = Date.now();
-    await setPullRequestStatus(pr, "pending", "Fetching");
+    await ghClient.setPullRequestStatus(pr, "pending", "Fetching");
     log(`   - fetching (target: '${jobFolder}')`);
     log("     - cloning");
     const git = simpleGit(jobFolder);
-    await git.clone(`https://${githubToken}@github.com/${githubOwner}/${githubRepo}`, jobFolder);
+    await git.clone(ghClient.cloneUrl, jobFolder);
     log(`     - checkout base (${pr.baseRef})`);
     await git.checkout(pr.baseRef);
     log(`     - merge head (${pr.headID})`);
     await git.merge([pr.headID]);
 
     // recheck if what we're doing still makes sense
-    if (await wasUpdated(pr)) { log("   - abort: the PR was updated"); return; }
-    if (!await isLastStatusOurs(pr)) { log("   - abort: looks like another worker handles this PR"); return; }
+    if (await ghClient.wasUpdated(pr)) { log("   - abort: the PR was updated"); return; }
+    if (!await ghClient.isLastStatusOurs(pr)) { log("   - abort: looks like another worker handles this PR"); return; }
 
     // job
     const timeStamp2 = Date.now();
-    await setPullRequestStatus(pr, "pending", "Running test job");
+    await ghClient.setPullRequestStatus(pr, "pending", "Running test job");
     log(`   - running test job`);
     const [pollOutput, resultPromise] = runCommand("npm install && npm test", jobFolder);
     const sendFeedback = async () => {
@@ -146,22 +89,22 @@ async function runJob(pr: PullRequest): Promise<void> {
     await sendFeedback();
 
     // recheck if what we're doing still makes sense
-    if (await wasUpdated(pr)) { log("   - abort: the PR was updated"); return; }
-    if (!await isLastStatusOurs(pr)) { log("   - abort: looks like another worker handles this PR"); return; }
+    if (await ghClient.wasUpdated(pr)) { log("   - abort: the PR was updated"); return; }
+    if (!await ghClient.isLastStatusOurs(pr)) { log("   - abort: looks like another worker handles this PR"); return; }
 
     // process error
     if (error) throw error;
 
     // process success
     const timeStamp3 = Date.now();
-    await setPullRequestStatus(pr, "success", `Success (git took ${(timeStamp2 - timeStamp1) / 1000 | 0}s, tests took ${(timeStamp3 - timeStamp2) / 1000 | 0}s)`);
+    await ghClient.setPullRequestStatus(pr, "success", `Success (git took ${(timeStamp2 - timeStamp1) / 1000 | 0}s, tests took ${(timeStamp3 - timeStamp2) / 1000 | 0}s)`);
     log(`     - success`);
 
     // recheck if we just marked an updated PR as succeeded
-    if (await wasUpdated(pr)) { await setPullRequestStatus(pr, "pending", `Stall. Just set status of an updated PR.`); log(`     - stall`); }
+    if (await ghClient.wasUpdated(pr)) { await ghClient.setPullRequestStatus(pr, "pending", `Stall. Just set status of an updated PR.`); log(`     - stall`); }
   } catch (e) {
     // at least try notifying about failure
-    try { await setPullRequestStatus(pr, "failure", ("" + e).slice(0, 1000)); } catch (_) { }
+    try { await ghClient.setPullRequestStatus(pr, "failure", ("" + e).slice(0, 1000)); } catch (_) { }
     log(`     - error (${e})`);
     throw e;
   }
@@ -170,43 +113,47 @@ async function runJob(pr: PullRequest): Promise<void> {
 async function main() {
   log("CI job info");
   log(` - CI identifier: ${ciIdentifier}`);
-  log(` - GitHub repo: ${githubOwner}/${githubRepo}`);
   log(` - using tmp folder: ${tmpFolder}`);
 
   const prs: { [prNumber: number]: Date } = {};
 
   while (true) {
-    try {
-      log("Polling PRs");
-      const prs = await getPullRequests();
+    let didAnything = false; // for backing off
+    for (const githubRepo of githubRepos) {
+      const ghClient = new GitHubCiClient(ciIdentifier, jobUid, githubOwner, githubRepo, githubToken);
+      log(`Polling PRs of ${githubOwner}/${githubRepo}`);
+      const prs = await ghClient.getPullRequests();
       for (const pr of prs) {
-        if (prs[pr.number] && prs[pr.number].updatedAt === pr.updatedAt) continue; // seen that PR?
-        log(` - PR #${pr.number} ('${pr.title}')`);
-        const status = await getJobStatus(pr);
-        if (!status || status.updatedAt < pr.updatedAt) {
-          log("   - classification: " + (status ? "updated" : "new"));
-          await runJob(pr);
-          prs[pr.number] = pr;
-        }
-        else if (status.state === "success" || status.state === "failure") {
-          log(`   - classification: done (${status.state})`);
-          prs[pr.number] = pr;
-        }
-        else if (status.state === "pending" && Date.now() - status.updatedAt.getTime() < ciStatusTimeoutMs) {
-          log("   - classification: looks active (pending)");
-        }
-        else if (status.state === "pending") {
-          log("   - classification: looks stuck (pending)");
-          await runJob(pr);
-          prs[pr.number] = pr;
+        try {
+          if (prs[pr.number] && prs[pr.number].updatedAt === pr.updatedAt) continue; // seen that PR?
+          log(` - PR #${pr.number} ('${pr.title}')`);
+          const status = await ghClient.getJobStatus(pr);
+          if (!status || status.updatedAt < pr.updatedAt) {
+            log("   - classification: " + (status ? "updated" : "new"));
+            await runJob(ghClient, pr);
+            prs[pr.number] = pr;
+            didAnything = true;
+          }
+          else if (status.state === "success" || status.state === "failure") {
+            log(`   - classification: done (${status.state})`);
+            prs[pr.number] = pr;
+          }
+          else if (status.state === "pending" && Date.now() - status.updatedAt.getTime() < ciStatusTimeoutMs) {
+            log("   - classification: looks active (pending)");
+          }
+          else if (status.state === "pending") {
+            log("   - classification: looks stuck (pending)");
+            await runJob(ghClient, pr);
+            prs[pr.number] = pr;
+            didAnything = true;
+          }
+        } catch (e) {
+          console.error(e);
+          log("Skipping PR...");
         }
       }
-    } catch (e) {
-      console.error(e);
-      log("Will retry...");
     }
-
-    await delay(10000);
+    await delay(didAnything ? 10000 : 60000);
   }
 }
 
---
+++ github.ts
@@ -0,0 +1,87 @@
+import { arch, platform, release, tmpdir } from "os";
+import { RequestAPI, UriOptions, UrlOptions } from "request";
+import { defaults as request_defaults, RequestPromise, RequestPromiseOptions } from "request-promise-native";
+
+export type PullRequest = { updatedAt: Date, number: number, title: string, baseRef: string, baseID: string, headID: string };
+export type State = "success" | "pending" | "failure";
+export type Status = { updatedAt: Date, state: State, description: string, url: string };
+export type Statuses = { [jobName: string]: Status };
+
+function parsePR(pr: any): PullRequest {
+  return {
+    updatedAt: new Date(pr.updated_at),
+    number: pr.number,
+    title: pr.title,
+    baseRef: pr.base.ref,
+    baseID: pr.base.sha,
+    headID: pr.head.sha
+  };
+}
+
+export class GitHubCiClient {
+  private request: RequestAPI<RequestPromise, RequestPromiseOptions, UriOptions | UrlOptions>;
+  private statusPrefix: string;
+
+  public constructor(
+    private readonly ciIdentifier: string,
+    readonly jobUid: string,
+    private readonly githubOwner: string,
+    private readonly githubRepo: string,
+    readonly githubToken: string
+  ) {
+    this.request = request_defaults({
+      headers: {
+        "User-Agent": "AutoRest CI",
+        "Authorization": "token " + githubToken
+      }
+    });
+    this.statusPrefix = `[${jobUid}] `;
+  }
+
+  public async getPullRequests(): Promise<PullRequest[]> { // https://developer.github.com/v3/pulls/#list-pull-requests
+    return JSON.parse(await this.request.get(`https://api.github.com/repos/${this.githubOwner}/${this.githubRepo}/pulls`)).map(parsePR);
+  }
+  public async getPullRequest(prNumber: number): Promise<PullRequest> { // https://developer.github.com/v3/pulls/#get-a-single-pull-request
+    return parsePR(JSON.parse(await this.request.get(`https://api.github.com/repos/${this.githubOwner}/${this.githubRepo}/pulls/${prNumber}`)));
+  }
+  public async getPullRequestStatuses(pr: PullRequest): Promise<Statuses> { // https://developer.github.com/v3/repos/statuses/#get-the-combined-status-for-a-specific-ref
+    const res = await this.request.get(`https://api.github.com/repos/${this.githubOwner}/${this.githubRepo}/commits/${pr.headID}/status`);
+    const statuses = JSON.parse(res).statuses;
+    const result: Statuses = {};
+    for (const status of statuses) {
+      result[status.context] = {
+        updatedAt: new Date(status.updated_at),
+        state: status.state,
+        description: status.description,
+        url: status.target_url
+      };
+    }
+    return result;
+  }
+  public async setPullRequestStatus(pr: PullRequest, state: State, description: string, url?: string): Promise<void> { // https://developer.github.com/v3/repos/statuses/#create-a-status
+    const body: any = {};
+    body.state = state;
+    if (url) body.target_url = url;
+    if (description) body.description = this.statusPrefix + description;
+    body.context = this.ciIdentifier;
+    const res = await this.request.post(`https://api.github.com/repos/${this.githubOwner}/${this.githubRepo}/statuses/${pr.headID}`, { body: JSON.stringify(body) });
+  }
+
+  public async getJobStatus(pr: PullRequest): Promise<Status | undefined> {
+    const statuses = await this.getPullRequestStatuses(pr);
+    return statuses[this.ciIdentifier];
+  }
+
+  public async isLastStatusOurs(pr: PullRequest): Promise<boolean> {
+    const status = await this.getJobStatus(pr);
+    return status && status.description.startsWith(this.statusPrefix);
+  }
+
+  public async wasUpdated(pr: PullRequest): Promise<boolean> {
+    return (await this.getPullRequest(pr.number)).updatedAt.getTime() !== pr.updatedAt.getTime();
+  }
+
+  public get cloneUrl(): string {
+    return `https://${this.githubToken}@github.com/${this.githubOwner}/${this.githubRepo}`
+  }
+}
